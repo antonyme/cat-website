c0141bdfe25d2b6d4765ee237bfe5511a1e4a9d7
antonyme <guillaumeduliscouet@gmail.com>
2019-03-24 20:09:25 +0100

    add dependencies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import Toasted from 'vue-toasted'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+Vue.use(Toasted, {
+  position: 'bottom-right',
+  theme: 'bubble',
+  duration: 3000,
+  singleton: true
+})
 
 Vue.config.productionTip = false
 
